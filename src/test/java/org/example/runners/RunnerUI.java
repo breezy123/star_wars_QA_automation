c38cdeb0458c4aecc2a4763d61a8d2c6730ee248
Aubrey Fredericks <AubreyFredericks@woolworths.co.za>
2024-04-22 00:28:56 +0200

    updated Cucumber report. No longer generating Extent report

--- src/test/java/org/example/runners/RunnerUI.java
+++ src/test/java/org/example/runners/RunnerUI.java
@@ -3,6 +3,7 @@ package org.example.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
 //        monochrome = true,
@@ -10,8 +11,10 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/test/resources/features"},
         tags="@ui",
         glue = {"org.example"},
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+        plugin = {"json:target/RunCucumber/cucumber.json",
+                "pretty","html:target/RunCucumber/OrangeCRM.html"}
 )
 
+@Test
 public class RunnerUI extends AbstractTestNGCucumberTests{
 }
