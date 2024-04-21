effca6a366daea2986cf5d1c1fa58598807e8e71
Aubrey Fredericks <AubreyFredericks@woolworths.co.za>
2024-04-22 00:18:19 +0200

    removed configured directory causes confusion/null values in Selenium driver.

--- src/main/java/org/example/UI/driver/SeleniumDriver.java
+++ src/main/java/org/example/UI/driver/SeleniumDriver.java
@@ -31,19 +31,19 @@ public class SeleniumDriver {
     public void launchDriver() {
         try {
             Properties p = new Properties();
-            p.load(Files.newInputStream(Paths.get(rootDirectory + "\\src\\test\\resources\\config\\config.props")));
+//            p.load(Files.newInputStream(Paths.get(rootDirectory + "\\src\\test\\resources\\config\\config.props")));
             switch (currentBrowser) {
                 case CHROME:
                     ChromeOptions chromeOptions = new ChromeOptions();
                     WebDriverManager.chromedriver().setup();
-                    System.setProperty(p.getProperty("WebDriverchrome"),p.getProperty("Chrome_Driver"));
+//                    System.setProperty(p.getProperty("WebDriverchrome"),p.getProperty("Chrome_Driver"));
                     chromeOptions.addArguments("--headless=new");
                     this.driver = new ChromeDriver(chromeOptions);
                     break;
                 case MS_EDGE:
                     EdgeOptions edgeOptions = new EdgeOptions();
                     WebDriverManager.edgedriver().setup();
-                    System.setProperty(p.getProperty("WebDriver_MS_Edge"),p.getProperty("MS_Edge_Driver"));
+//                    System.setProperty(p.getProperty("WebDriver_MS_Edge"),p.getProperty("MS_Edge_Driver"));
                     edgeOptions.addArguments("--guest");
                     edgeOptions.addArguments("--remote-allow-origins=*");
                     edgeOptions.addArguments("-headless");
